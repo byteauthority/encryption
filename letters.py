97c127f11e31a35971699ace372fdf72b62a18f5
byteauthority <154011817+byteauthority@users.noreply.github.com>
2025-05-01 21:38:55 +0200

    zip working

--- letters.py
+++ letters.py
@@ -1,4 +1,4 @@
-seq = "AAAAABBBBCCCDDE"
+seq = "AAAAABBBBCCCDDEEF"
 
 
 l = list(seq)
@@ -33,21 +33,40 @@ l = list(seq)
 # print (n)
 
 
-count = 0
+# count = 0
 
+# n = []
+# for i in range (len(seq)+1):
+#     if i == 0:
+#         count+=1
+#     elif i == len(seq):
+#         if seq[i] == seq[i-1]:
+#             count+=1
+#         else:
+#             count = 1
+#             n.append(f"{seq[i]}" + f"{count}")
+#     elif i > 0 and i < len(seq):
+#         if seq[i] == seq[i-1]:
+#             count+=1
+#         else:
+#             n.append(f"{seq[i-1]}" + f"{count}")
+#             count = 1
+    
+# print (n)
+    
+
+
+count = 0
 n = []
-for i in range (len(seq)):
+for i in range(len(seq)):
+    print(seq[i])
+
     if i == 0:
-        count+=1
-    elif i == len(seq):
-        count+=1
-        n.append(f"{l[i]}" + f"{count}")
-    elif i > 0 and i < len(seq):
+        count = 1
+    elif i > 0:
         if seq[i] == seq[i-1]:
             count+=1
-        else:
-            n.append(f"{l[i-1]}" + f"{count}")
+        elif seq[i] != seq[i-1]:
+            n.append(f"{seq[i]}" + f"{count}")
             count = 1
-    
-print (n)
-    
+print(n)
---
+++ zip.py
@@ -0,0 +1,25 @@
+data = 'AAAABBBBBCCC' 
+count = 0
+new = []
+for i in range (len(data)):
+    if i == 0:
+        count = 1
+
+    elif len(data)-1 == i:
+        if data[i] != data[i-1]:
+            new.append(f'{data[i-1]}'+f'{count}')
+            # print(f'{data[i-1]}'+f'{count}')
+            count = 1
+            new.append(f'{data[i]}'+f'{count}')
+            # print(f'{data[i]}'+f'{count}')
+        else:
+            count+=1
+            new.append(f'{data[i]}'+f'{count}')
+            # print(f'{data[i]}'+f'{count}')
+    elif i > 0 and data[i] == data[i-1]:
+        count+=1
+    elif i > 0 and data[i] != data[i-1]:
+        new.append(f'{data[i-1]}'+f'{count}')
+        # print(f'{data[i-1]}'+f'{count}')
+        count = 1
+print(''.join(new))
