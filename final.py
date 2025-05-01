1328f4884ba6d6052b731db7530704ce76a32c51
byteauthority <154011817+byteauthority@users.noreply.github.com>
2025-05-01 21:54:13 +0200

    "unzip" is working if the number is lessorequal to 9

--- final.py
+++ final.py
@@ -1,4 +1,3 @@
-data = 'AAAABBBBBCCC' 
 
 def zip(data):  
     count = 0
@@ -25,3 +24,19 @@ def zip(data):
             # print(f'{data[i-1]}'+f'{count}')
             count = 1
     return (''.join(new))
+
+def unzip(zip):
+    unzip = []
+
+    for i in range(0,len(zip),2):
+        unzip.append (f'{zip[i]}'* int(zip[i+1]))
+
+    unzip = ''.join(unzip)
+    return (unzip)
+
+user = input("Enter smth to compress: ")
+print(zip(user))
+
+
+user = input("Input smth to expand: ")
+print (unzip(user))
--- unzip.py
+++ unzip.py
@@ -1,1 +1,8 @@
-zip = 'A4B5C3'
+zip = 'A4B5N7K8'
+unzip = []
+
+for i in range(0,len(zip),2):
+    unzip.append (f'{zip[i]}'* int(zip[i+1]))
+
+unzip = ''.join(unzip)
+print(unzip)
